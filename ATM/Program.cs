334daca1bb0e03c90d4f4a636c8aafe6bef854ba
Anuththara Perera <anuththaraperera14@gmail.com>
2024-07-30 11:41:09 +0530

    Add project files.

---
+++ ATM/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class cardHolder {
+    String cardNumber;
+    int pin;
+    String firstName;
+    String lastName;
+    double balance;
+
+    public cardHolder(string cardNum, int pin, String firstName, String lastName, double balance)
+    {
+        this.cardNumber = cardNum;
+        this.pin = 0;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.balance = balance;
+            
+    }
+}
